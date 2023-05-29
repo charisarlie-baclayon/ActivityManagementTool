d72b1e5cfa931943ab03149a67d4ca04a2725af3
James-Lewel <jameslewel.padecio@cit.edu>
2023-05-30 06:06:21 +0800

    Add Student Page

---
+++ frontend/src/pages/Student/StudentViewActivities.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import VerticalNavBar from '../../assets/common/vertical-nav-bar'
+
+export const StudentViewActivities = () => {
+  return (
+    <div>
+        <div className='page-content'>
+        <h1>Henlo World!</h1>
+        <p>HIIII</p>
+        </div>
+    </div>
+  )
+}
