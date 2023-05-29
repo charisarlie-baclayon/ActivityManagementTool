2c1505a9bcf153d274ac596b4bdaa5e3d0cc08a1
Kid Omar Costelo <kidomar.costelo@cit.ed>
2023-05-30 03:54:23 +0800

    Add initial create activities

---
+++ frontend/src/pages/Teacher/CreateActivities.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import VerticalNavBar from '../../assets/common/vertical-nav-bar'
+
+export const CreateActivities = () => {
+  return (
+    <>
+        <h1>Create Teacher</h1>
+    </>
+  )
+}
