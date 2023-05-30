8f33756e869167bebdeeb8cf8cc007071348e16e
James-Lewel <jameslewel.padecio@cit.edu>
2023-05-30 11:27:21 +0800

    Add Temporary Create Activities

--- frontend/src/pages/Teacher/CreateActivities.jsx
+++ frontend/src/pages/Teacher/CreateActivities.jsx
@@ -1,10 +1,125 @@
-import React from 'react'
-import VerticalNavBar from '../../assets/common/vertical-nav-bar'
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
 
 export const CreateActivities = () => {
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [link, setLink] = useState('');
+  const [showModal, setShowModal] = useState(false);
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleDescriptionChange = (event) => {
+    setDescription(event.target.value);
+  };
+
+  const handleLinkChange = (event) => {
+    setLink(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Perform the necessary actions with the form data
+    // e.g., create a new activity using an API call
+
+    // Reset the form fields
+    setName('');
+    setDescription('');
+    setLink('');
+
+    // Close the modal after form submission
+    setShowModal(false);
+  };
+
+  const handleModalClose = () => {
+    setShowModal(false);
+  };
+
+  const handleModalShow = () => {
+    setShowModal(true);
+  };
+
   return (
     <>
-        <h1>Create Teacher</h1>
+      <h1>Create Activity</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+          <label htmlFor="name">Name:</label>
+          <br />
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={handleNameChange}
+            required
+          />
+        </div>
+        <div className="input-container">
+          <label htmlFor="description">Description:</label>
+          <br />
+          <input
+            type="text"
+            id="description"
+            value={description}
+            onChange={handleDescriptionChange}
+            required
+          />
+        </div>
+        <div className="input-container">
+          <label htmlFor="link">Link:</label>
+          <br />
+          <input
+            type="text"
+            id="link"
+            value={link}
+            onChange={handleLinkChange}
+            required
+          />
+        </div>
+        <button type="submit">Create</button>
+      </form>
+
+      <Button variant="primary" onClick={handleModalShow}>
+        Launch demo modal
+      </Button>
+
+      <Modal show={showModal} onHide={handleModalClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="name@example.com"
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Example textarea</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleModalClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleModalClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
-  )
-}
+  );
+};
