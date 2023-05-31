fec3e0a336dda3d55363e65665c2c54ee5a0a898
CHARIS ARLIE BACLAYON <charisarlie.baclayon@cit.edu>
2023-05-31 19:33:38 +0800

    Add choices in status

--- backend/amt/models.py
+++ backend/amt/models.py
@@ -2,13 +2,19 @@ from django.db import models
 
 # Create your models here.
 class Activity(models.Model):
+    STATUS_CHOICES = [
+        ('INCOMPLETE', 'Incomplete'),
+        ('COMPLETE', 'Complete'),
+        ('IN_PROGRESS', 'In Progress'),
+    ]
+
     name = models.CharField(max_length=200, default="", null=False)
     description = models.CharField(max_length=5000, default="", null=False)
-    link = models.URLField(max_length=200, default="", null=True) 
-    status = models.CharField(max_length=100, default="INCOMPLETE")
+    link = models.URLField(max_length=200, default="", null=True)
+    status = models.CharField(max_length=100, choices=STATUS_CHOICES, default="INCOMPLETE")
 
     def __str__(self):
-        return self.name + ' ' + self.description + ' ' + self.link
+        return self.name + ' ' + self.description + ' ' + self.link + ' ' + self.status
 
     class Meta:
         db_table = 'Activities'
