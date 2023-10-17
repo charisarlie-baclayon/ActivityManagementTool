37e1b246e1dc2d14c9d6057c8a1cdc0de7efabdc
CHARIS ARLIE BACLAYON <charisarlie.baclayon@cit.edu>
2023-10-18 05:59:40 +0800

    Change field name

--- backend/amt/models/Template.py
+++ backend/amt/models/Template.py
@@ -2,6 +2,6 @@ from django.db import models
 from amt.models.Category import Category
 
 class Template(models.Model):
-    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
     description = models.TextField(max_length=10000)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
