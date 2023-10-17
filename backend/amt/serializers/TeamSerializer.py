fe92a1263243a30bdc654d37d68a3a1eed2fb349
CHARIS ARLIE BACLAYON <charisarlie.baclayon@cit.edu>
2023-10-18 06:10:42 +0800

    Fix team serializer

--- backend/amt/serializers/TeamSerializer.py
+++ backend/amt/serializers/TeamSerializer.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
 from amt.models import Team
+from amt.serializers.StudentSerializer import StudentSerializer
 
 
 class TeamSerializer(serializers.ModelSerializer):
-
+    students = StudentSerializer(many=True, read_only=True, source='student_set')
     class Meta:
         model = Team
         fields = ('__all__')
