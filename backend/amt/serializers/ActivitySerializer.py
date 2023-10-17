1e8ba28b771dac507bd49e6e21fd8a1ac998cd18
CHARIS ARLIE BACLAYON <charisarlie.baclayon@cit.edu>
2023-10-18 05:51:25 +0800

    Update relations activity work and comment

--- backend/amt/serializers/ActivitySerializer.py
+++ backend/amt/serializers/ActivitySerializer.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 from amt.models import Activity
+from amt.serializers.CommentSerializer import CommentSerializer
+from amt.serializers.WorkSerializer import WorkSerializer
 
 
 class ActivitySerializer(serializers.ModelSerializer):
+
+    works = WorkSerializer(many=True, read_only=True, source='work_set')
+    comments = CommentSerializer(many=True, read_only=True, source='comment_set')
 
     class Meta:
         model = Activity
