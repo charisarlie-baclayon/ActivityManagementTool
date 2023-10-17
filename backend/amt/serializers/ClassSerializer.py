6a50630aa9169ea6a68e5e16eda775ac48103ae9
CHARIS ARLIE BACLAYON <charisarlie.baclayon@cit.edu>
2023-10-18 06:07:55 +0800

    Fix serializer class

--- backend/amt/serializers/ClassSerializer.py
+++ backend/amt/serializers/ClassSerializer.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
 from amt.models import Class
+from amt.serializers.TeamSerializer import TeamSerializer
 
 
 class ClassSerializer(serializers.ModelSerializer):
+    teams = TeamSerializer(many=True, read_only=True, source='team_set')
 
     class Meta:
         model = Class
