28a108ddb95cbb4aeef57c3800f11bc2ef824a3c
jeremyjulesflores <jeremyjulesflores1@gmail.com>
2023-10-20 03:45:06 +0800

    Permission Role -> Groups Used Role but will be using groups

--- backend/amt/permissions/permissions.py
+++ backend/amt/permissions/permissions.py
@@ -2,4 +2,4 @@ from rest_framework import permissions
 
 class IsTeacher(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.role.filter(name = 'Teacher').exists()
+        return request.user.groups.filter(name = 'Teacher').exists()
