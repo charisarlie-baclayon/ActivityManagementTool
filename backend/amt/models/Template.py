82aabbb9570a4e2118b2d032159ac3bcf04cea3d
jeremyjulesflores <jeremyjulesflores1@gmail.com>
2023-10-20 04:29:22 +0800

    Remove FK for the meantime for template

--- backend/amt/models/Template.py
+++ backend/amt/models/Template.py
@@ -4,4 +4,5 @@ from amt.models.Category import Category
 class Template(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=10000)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    #category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    
