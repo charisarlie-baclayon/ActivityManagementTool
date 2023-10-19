3bb6bb9e5c2832ad43445d6a68e62ffcd9dc6e68
jeremyjulesflores <jeremyjulesflores1@gmail.com>
2023-10-20 03:14:19 +0800

    Update

--- backend/amt/controllers/TeamController.py
+++ backend/amt/controllers/TeamController.py
@@ -8,7 +8,7 @@ from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from amt.permissions.permissions import IsTeacher
 from django.contrib.auth.models import Group
-from amt.models import Student
+# from amt.models import Student
 
 from amt.models import Team
 from amt.serializers import TeamSerializer
@@ -27,18 +27,18 @@ class TeamController(GenericViewSet, ListModelMixin, RetrieveModelMixin, CreateM
         
     
 
-    @action(detail=True, methods=['post'])
-    def add_members(self, request, pk=None):
-        try:
-            team = self.get_object()
-            student_emails = request.data.get('student_emails', [])
+    # @action(detail=True, methods=['post'])
+    # def add_members(self, request, pk=None):
+    #     try:
+    #         team = self.get_object()
+    #         student_emails = request.data.get('student_emails', [])
 
-            # Filter students by email and get their IDs
-            student_ids = list(Student.objects.filter(email__in=student_emails).values_list('id', flat=True))
+    #         # Filter students by email and get their IDs
+    #         student_ids = list(Student.objects.filter(email__in=student_emails).values_list('id', flat=True))
 
-            # Associate the selected students with the team by updating their team_id
-            Student.objects.filter(id__in=student_ids).update(team_id=team.id)
+    #         # Associate the selected students with the team by updating their team_id
+    #         Student.objects.filter(id__in=student_ids).update(team_id=team.id)
 
-            return Response({'success': True, 'message': 'Members added successfully'}, status=status.HTTP_200_OK)
-        except Team.DoesNotExist:
-            return Response({'success': False, 'message': 'Team not found'}, status=status.HTTP_404_NOT_FOUND)
+    #         return Response({'success': True, 'message': 'Members added successfully'}, status=status.HTTP_200_OK)
+    #     except Team.DoesNotExist:
+    #         return Response({'success': False, 'message': 'Team not found'}, status=status.HTTP_404_NOT_FOUND)
--- backend/amt/models/Team.py
+++ backend/amt/models/Team.py
@@ -1,6 +1,6 @@
 from django.db import models
 from amt.models.Class import Class
-from amt.models.Student import Student
+# from amt.models.Student import Student
 
 class Team(models.Model):
     name = models.CharField(max_length=255)
@@ -8,5 +8,5 @@ class Team(models.Model):
     team_class = models.ForeignKey(Class, on_delete=models.CASCADE)
     #leader = models.ForeignKey(Student, on_delete=models.SET_NULL, null=True, blank=True, related_name='leading_teams')
 
-    def members(self):
-        return Student.objects.filter(team_id=self.pk)
+    # def members(self):
+    #     return Student.objects.filter(team_id=self.pk)
