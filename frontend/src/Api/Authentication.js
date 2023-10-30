ae45c6bee3323c5ff409e3a1c907fbd40a8bb753
Kid Omar Costelo <kidomar.costelo@cit.edu>
2023-10-30 18:29:02 +0800

    Refactor authentication endpoints to store token

--- frontend/src/Api/Authentication.js
+++ frontend/src/Api/Authentication.js
@@ -1,66 +1,53 @@
-import axios from "axios";
+import { apiSlice } from "./apiSlice";
 
-export const loginTeacher = async (email, password) => {
-    try {
-        const response = await axios.post("http://127.0.0.1:8000/api/teachers/login/", {
-            email,
-            password,
-        });
-        console.log(response.data);
-        return response.data;
-    } catch (error) {
-        console.log(error.response.data);
-        throw error;
-    }
-}
+export const Authentication = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        loginTeacher: builder.mutation({
+            query: (credentials) => ({
+                url: "/api/teachers/login/",
+                method: "POST",
+                body: { ...credentials },
+            }),
+        }),
+        loginStudent: builder.mutation({
+            query: (credentials) => ({
+                url: "/api/students/login/",
+                method: "POST",
+                body: { ...credentials },
+            }),
+        }),
+        acquireToken: builder.mutation({
+            query: (credentials) => ({
+                url: "/tokens/acquire",
+                method: "POST",
+                body: { ...credentials },
+            }),
+        }),
+        refreshToken: builder.mutation({
+            query: (refreshToken) => ({
+                url: "/tokens/refresh",
+                method: "POST",
+                body: {
+                    refresh: refreshToken,
+                },
+            }),
+        }),
+        verifyToken: builder.mutation({
+            query: (token) => ({
+                url: "/tokens/verify",
+                method: "POST",
+                body: {
+                    token,
+                },
+            }),
+        }),
+    }),
+});
 
-export const loginStudent = async (email, password) => {
-    try {
-        const response = await axios.post("http://127.0.0.1:8000/api/students/login/", {
-          email,
-          password,
-        });
-
-        return response.data;
-    } catch (error) {
-        console.log(error.response.data);
-        throw error;
-    }
-}
-
-export const acquireToken = async (email, password) => {
-    try {
-        const response = await axios.post("http://127.0.0.1:8000/tokens/acquire", {
-        email,
-        password,
-        });
-        return response.data;
-    } catch (error) {
-        console.log(error.response.data);
-        throw error;
-    }
-};
-
-export const refreshToken = async (refreshToken) => {
-    try {
-      const response = await axios.post("http://127.0.0.1:8000/tokens/refresh", {
-        refresh: refreshToken,
-      });
-      return response.data;
-    } catch (error) {
-      console.log(error.response.data);
-      throw error;
-    }
-};
-  
-export const verifyToken = async (token) => {
-    try {
-      const response = await axios.post("http://127.0.0.1:8000/tokens/verify", {
-        token,
-      });
-      return response.statusText === "OK" ? true : false;
-    } catch (error) {
-      console.log(error.response.data);
-      throw error;
-    }
-};
+export const {
+    useLoginTeacherMutation,
+    useLoginStudentMutation,
+    useAcquireTokenMutation,
+    useRefreshToken,
+    useVerifyTokenMutation,
+} = Authentication;
---
+++ frontend/src/features/auth/PrivateRoute.jsx
@@ -0,0 +1,58 @@
+import { useEffect } from "react";
+import {
+    Route,
+    Navigate,
+    Outlet,
+    useLocation,
+    useNavigate,
+} from "react-router-dom";
+import { useLocalStorage } from "../../hooks/useLocalStorage";
+//import { verifyToken } from "../../api/Authentication";
+import { useSelector } from "react-redux";
+import { selectCurrentToken } from "../../features/auth/authSlice";
+import { useVerifyTokenMutation } from "../../api/Authentication";
+
+export const PrivateRoutes = () => {
+    //const [access, setAccess] = useLocalStorage("access", "");
+    const [role] = useLocalStorage("role", "");
+    const location = useLocation();
+    const token = useSelector(selectCurrentToken);
+    // const token =
+    //     "1eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTcwNTU2MTA5MywiaWF0IjoxNjk3Nzg1MDkzLCJqdGkiOiIyNjIzMzhiZjE1MzM0ZjhiOTQzZWEzYzFmZTdiZmY3MCIsInVzZXJfaWQiOjMsImVtYWlsIjoic3R1ZGVudEBlbWFpbC5jb20ifQ.c8JnHvNUdOC8bBn8fwjq6mQhWRoUH0fxYhBCCJ7kwY8";
+    const navigate = useNavigate();
+    const [verifyToken] = useVerifyTokenMutation();
+    useEffect(() => {
+        const verifyAccessToken = async () => {
+            if (token) {
+                try {
+                    const verificationResponse = await verifyToken(token);
+                    if (!verificationResponse) {
+                        console.log("di ka authorized");
+                        navigate("/");
+                    }
+                } catch (error) {
+                    console.log("di ka authorized");
+                    console.error(error);
+                    navigate("/");
+                }
+            }
+        };
+
+        verifyAccessToken();
+    }, [token, navigate]);
+
+    if (!token) {
+        console.log("no token");
+        return <Navigate to='/' />;
+    }
+
+    if (role === "teacher" && location.pathname.startsWith("/teacher")) {
+        return <Outlet />;
+    }
+
+    if (role === "student" && location.pathname.startsWith("/student")) {
+        return <Outlet />;
+    }
+
+    return <Navigate to={`/${role}`} />;
+};
