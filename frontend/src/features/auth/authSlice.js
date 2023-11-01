3eeaa4ec5e834ec993757ab5f74eadcdc99b3cdb
Kid Omar Costelo <kidomar.costelo@cit.edu>
2023-11-01 15:49:48 +0800

    Add persistent login implementation

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -2,29 +2,26 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const authSlice = createSlice({
   name: "auth",
-  initialState: { user: null, token: null },
+  initialState: { user: null, token: null, role: null },
   reducers: {
     setCredentials: (state, action) => {
-      const { user, accessToken } = action.payload;
+      const { user, accessToken, role } = action.payload;
       state.user = user;
+      state.role = role;
       state.token = accessToken;
     },
     logOut: (state) => {
       state.user = null;
       state.token = null;
+      state.role = null;
     },
   },
 });
 
-export const {
-  setCredentials,
-  logOut,
-  loginSuccess,
-  loginFailure,
-  logoutSuccess,
-} = authSlice.actions;
+export const { setCredentials, logOut } = authSlice.actions;
 
 export default authSlice.reducer;
 
 export const selectCurrentUser = (state) => state.auth.user;
 export const selectCurrentToken = (state) => state.auth.token;
+export const selecCurrentRole = (state) => state.auth.role;
