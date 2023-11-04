7ac78a20851482b1b9ee73e10351f75a71c4142c
Robert William Wacan <robertwilliam.wacan@cit.edu>
2023-11-05 02:37:39 +0800

    Add hook for FetchStudentByTeam

--- frontend/src/hooks/useStudent.jsx
+++ frontend/src/hooks/useStudent.jsx
@@ -1,9 +1,18 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { selectCurrentToken } from "../features/auth/authSlice";
-import { readStudent, readStudents, createStudent, deleteStudent, updateStudent } from "../api/Students";
+import { 
+    useReadStudentsMutation,
+    useReadStudentsByTeamMutation,
+    useReadStudentMutation,
+    useCreateStudentMutation,
+    useDeleteStudentMutation,
+    useUpdateStudentMutation,
+} from "../api/Student";
+
 
 export function useFetchStudent(id) {
+    const [readStudent] = useReadStudentMutation();
     const [studentData, setStudentData] = useState(null);
     const accessToken = useSelector(selectCurrentToken);
     
@@ -26,6 +35,7 @@ export function useFetchStudent(id) {
 }
 
 export function useFetchStudents() {
+    const [readStudents] = useReadStudentsMutation();
     const [students, setStudents] = useState([]);
     const accessToken = useSelector(selectCurrentToken);
 
@@ -45,7 +55,29 @@ export function useFetchStudents() {
     return students;
 }
 
+export function useFetchStudentsByTeam(id) {
+    const [readStudentsByTeam] = useReadStudentsByTeamMutation();
+    const [students, setStudents] = useState([]);
+    const accessToken = useSelector(selectCurrentToken);
+
+    useEffect (() => {
+        const fetchStudentsByTeam = async () => {
+            try {
+                const response = await readStudentsByTeam(id,accessToken);
+                setStudents(response);
+            } catch (error) {
+                console.log(error.response);
+            }
+        };
+
+        fetchStudentsByTeam();
+    }, [accessToken]);
+
+    return students;
+}
+
 export function useCreateStudent() {
+    const [createStudent] = useCreateStudentMutation();
     const accessToken = useSelector(selectCurrentToken);
 
     const createNewStudent = async (data) => {
@@ -61,6 +93,7 @@ export function useCreateStudent() {
 }
 
 export function useUpdateStudent() {
+    const [updateStudent] = useUpdateStudentMutation();
     const accessToken = useSelector(selectCurrentToken);
 
     const updateExistingStudent = async (id, data) => {
@@ -76,6 +109,7 @@ export function useUpdateStudent() {
 }
 
 export function useDeleteStudent() {
+    const [deleteStudent] = useDeleteStudentMutation();
     const accessToken = useSelector(selectCurrentToken);
 
     const deleteStudentById = async (id) => {
