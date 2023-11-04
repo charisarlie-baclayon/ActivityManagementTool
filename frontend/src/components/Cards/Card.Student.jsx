6a3b2e69a00c5548898642ee53467c05612ac982
Robert William Wacan <robertwilliam.wacan@cit.edu>
2023-11-05 02:36:01 +0800

    Add Student Card

---
+++ frontend/src/components/Cards/Card.Student.jsx
@@ -0,0 +1,12 @@
+export const StudentCard = ({ studentData, onClick }) => {
+    return (
+      <div className="card p-3 shadow-sm clickable" onClick={onClick}>
+        <div className="card-body d-flex flex-column align-items-center gap-3">
+          <div className="bg-dark p-5 rounded-3"></div>
+          <h5 className="card-title">{studentData.user.first_name} {studentData.user.last_name}</h5>
+          <h6 className="card-text">Email: {studentData.user.email}</h6>
+          <h6 className="card-text">Role: {studentData.user.role}</h6>
+        </div>
+      </div>
+    );
+  };
