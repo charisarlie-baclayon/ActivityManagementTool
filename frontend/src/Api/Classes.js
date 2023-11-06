a26405cb80489a5ea7b55f8067f44a17ba184fb6
James-Lewel <jameslewel.padecio@cit.edu>
2023-11-06 22:09:56 +0800

    Update Classes API

--- frontend/src/Api/Classes.js
+++ frontend/src/Api/Classes.js
@@ -3,16 +3,10 @@ import { apiSlice } from "./apiSlice";
 export const Classes = apiSlice.injectEndpoints({
 	endpoints: (builder) => ({
 		readClasses: builder.mutation({
-			query: () => ({
-				url: "/api/classes/",
-				method: "GET",
-			}),
+			query: () => "/api/classes/",
 		}),
 		readClass: builder.mutation({
-			query: (id) => ({
-				url: `/api/classes/${id}/`,
-				method: "GET",
-			}),
+			query: (id) => `/api/classes/${id}/`,
 		}),
 		createClass: builder.mutation({
 			query: (data) => ({
@@ -20,21 +14,33 @@ export const Classes = apiSlice.injectEndpoints({
 				method: "POST",
 				body: { ...data },
 			}),
-		}),
-		deleteClass: builder.mutation({
-			query: (id) => ({
-				url: `/api/classes/${id}/`,
-				method: "DELETE",
-			}),
-		}),
-		updateClass: builder.mutation({
-			query: (data) => ({
-				url: `/api/classes/${data.id}/`,
-				method: "PUT",
-				body: { ...data },
-			}),
-		}),
-	}),
+		},
+			deleteClass: builder.mutation({
+				query: (id) => ({
+					url: `/api/classes/${id}/`,
+					method: "DELETE",
+				}),
+			},
+				updateClass: builder.mutation({
+					query: (data) => ({
+						url: `/api/classes/${data.id}/`,
+						method: "PUT",
+						body: { ...data },
+					}),
+				},
+					readClassesBySection: builder.mutation({
+						query: (section) => ({
+							url: `/api/classes/get_classes_by_section/?section=${section}`,
+							method: "GET",
+						}),
+					},
+						readClassesByCourse: builder.mutation({
+							query: (course_id) => ({
+								url: `/api/classes/get_classes_by_course/?course_id=${course_id}`,
+								method: "GET",
+							}),
+						},
+  }),
 });
 
 export const {
@@ -43,4 +49,6 @@ export const {
 	useCreateClassMutation,
 	useDeleteClassMutation,
 	useUpdateClassMutation,
+	useReadClassesBySectionMutation,
+	useReadClassesByCourseMutation,
 } = Classes;
