1dbd80e574109b3174d4fcfc3fec26c56c55408c
Robert William Wacan <robertwilliam.wacan@cit.edu>
2023-11-19 01:56:41 +0800

    Add delete work

--- frontend/src/components/popups/activity/student-edit-work.jsx
+++ frontend/src/components/popups/activity/student-edit-work.jsx
@@ -3,6 +3,7 @@ import Form from "react-bootstrap/Form";
 import { useNavigate } from "react-router-dom";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
+import { useDeleteWorkMutation, useUpdateWorkMutation } from "../../../Api/Work";
 
 export const EditWorkModal = ({ show, handleClose, editWorkData, onSubmit }) => {
     /*const [editData, setEditData] = useState({
@@ -22,6 +23,9 @@ export const EditWorkModal = ({ show, handleClose, editWorkData, onSubmit }) => 
       console.log('Inside useEffect. Received editWorkData:', editWorkData);
       setEditData(editWorkData || {});
     }, [editWorkData]);
+
+    const fileAttachmentName =
+    editData?.file_attachment && editData?.file_attachment.split("/").pop();
     
     console.log('Current editData:', editData);
 
@@ -37,42 +41,99 @@ export const EditWorkModal = ({ show, handleClose, editWorkData, onSubmit }) => 
 
     console.log("Current editData:", editData);
 
-    const handleDelete = (e) => {
-	};
+    const [deleteWork] = useDeleteWorkMutation();
+
+    const handleDelete = async (e) => {
+      const response = await deleteWork(editData.id);
+      console.log(editData.id);
+
+      console.log(response);
+      if (response) {
+        handleClose();
+        console.log("Successfully deleted work!");
+      } else {
+        console.log("Work delete failed.");
+      }
+
+      window.location.reload();
+	  };
+
+    // Update Work
+    const [updateWork] = useUpdateWorkMutation();
+
+    const handleUpdate = async (e) => {
+      const formData = new FormData();
+      formData.append("activity_id", editData.activity_id);
+      formData.append("work", editData.work);
+      formData.append("file_attachment", editData.file_attachment);
+
+      // Perform validation or additional logic if needed
+      console.log("Updating Work:", formData);
+      for (const entry of formData.entries()) {
+        console.log(entry);
+      }
+
+      const response = await updateWork({
+          id: editWorkData.id,  // Pass the id for completing the URL
+          formData: formData,      // Pass the formData for the request body
+      });
+
+      console.log(response);
+      if (response) {
+        handleClose();
+        console.log("Successfully updated work!");
+      } else {
+        console.log("Work update failed.");
+      }
+
+      //window.location.reload();
+    };
+
   
     return (
-      <Modal show={show} onHide={handleClose} size='lg' centered>
+      <Modal show={show} onHide={handleClose} size="lg" centered>
         <Modal.Header closeButton>
-          <Modal.Title className='fs-6 fw-bold'>Edit Work</Modal.Title>
+          <Modal.Title className="fs-6 fw-bold">Edit Work</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form className='d-flex flex-column gap-3'>
-            <Form.Group controlId='work-title-input'>
-              <Form.Label>Title</Form.Label>
+          <Form className="d-flex flex-column gap-3">
+            <Form.Group controlId="work-input">
+              <Form.Label>Work</Form.Label>
               <Form.Control
-                type='text'
-                name='title'
+                type="text"
+                name="work"
                 value={editData?.work}
                 onChange={handleChange}
               />
             </Form.Group>
-  
-            <Form.Group controlId='work-file-attachment-input'>
-            <Form.Label>File Attachment</Form.Label>
-            <span><br/>{editWorkData?.file_attachment && editWorkData?.file_attachment.split('/').pop()}</span>
-          </Form.Group>
-  
+
+            <Form.Group controlId="file-input">
+              <Form.Label>File Attachment</Form.Label>
+              {fileAttachmentName && (
+                <span>
+                  <br />
+                  {fileAttachmentName}
+                </span>
+              )}
+            </Form.Group>
+
             {/* Add more Form.Group elements for other fields as needed */}
           </Form>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant='outline-secondary' onClick={handleClose}>
+          <Button variant="outline-secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant='secondary' onClick={() => onSubmit(editData)}>
+          <Button 
+            variant="secondary" 
+            onClick={handleUpdate}
+          >
             Update Work
           </Button>
-          <Button variant='secondary' onClick={handleDelete()}>
+          <Button 
+            variant="danger" 
+            onClick={handleDelete}
+          >
             Delete Work
           </Button>
         </Modal.Footer>
