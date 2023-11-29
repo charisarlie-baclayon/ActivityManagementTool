a47a2354cb3fd128974c9eddf633b031d5e371be
Robert William Wacan <robertwilliam.wacan@cit.edu>
2023-11-30 01:08:13 +0800

    Add Delete and Update for Work

--- frontend/src/components/popups/activity/student-edit-work.jsx
+++ frontend/src/components/popups/activity/student-edit-work.jsx
@@ -3,9 +3,9 @@ import Form from "react-bootstrap/Form";
 import { useNavigate } from "react-router-dom";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
-import { useDeleteWorkMutation, useUpdateWorkMutation } from "../../../Api/Work";
+import { useReadWorkMutation,useDeleteWorkMutation, useUpdateWorkMutation } from "../../../Api/Work";
 
-export const EditWorkModal = ({ show, handleClose, editWorkData, onSubmit }) => {
+export const EditWorkModal = ({ show, handleClose, editWorkData, onSubmit, id, workId }) => {
     /*const [editData, setEditData] = useState({
         ...editWorkData,
         downloadLink: editWorkData?.file_attachment || '', // Assuming file_attachment is the URL
@@ -15,9 +15,15 @@ export const EditWorkModal = ({ show, handleClose, editWorkData, onSubmit }) => 
     const [editData, setEditData] = useState(editWorkData);
     console.log(editWorkData);
 */
+    const [workData, setWorkData] = useState({
+      work: "",
+      file_attachment: null,
+      activity_id: id,
+    });
 
     console.log(editWorkData);
     const [editData, setEditData] = useState(editWorkData || {});
+    const [isDataChanged, setIsDataChanged] = useState(false);
 
     useEffect(() => {
       console.log('Inside useEffect. Received editWorkData:', editWorkData);
@@ -27,20 +33,20 @@ export const EditWorkModal = ({ show, handleClose, editWorkData, onSubmit }) => 
     const fileAttachmentName =
     editData?.file_attachment && editData?.file_attachment.split("/").pop();
     
-    console.log('Current editData:', editData);
+    console.log('Current workData:', workData);
 
-    const downloadLink = editWorkData?.file_attachment;
-
+    /*
     const handleChange = (e) => {
       const { name, value } = e.target;
-      setEditData({
-        ...editData,
+      setWorkData({
+        ...workData,
         [name]: value,
       });
-    };
+    };*/
 
     console.log("Current editData:", editData);
 
+    //Delete Work
     const [deleteWork] = useDeleteWorkMutation();
 
     const handleDelete = async (e) => {
@@ -58,14 +64,84 @@ export const EditWorkModal = ({ show, handleClose, editWorkData, onSubmit }) => 
       window.location.reload();
 	  };
 
+    //Get work
+    const [getWork] = useReadWorkMutation();
+
+    useEffect(() => {
+      const handleGet = async (e) => {
+        try {
+          const response = await getWork(editData.id);
+          console.log(editData.id);
+      
+          console.log(response);
+          if (response) {
+            // Update workData with the response data
+            setWorkData({
+              work: response.data.work || "",
+              file_attachment: response.data.file_attachment || null,
+              activity_id: response.data.activity_id || id,
+            });
+            setEditData({
+              ...response.data,
+            });
+            
+            console.log("Successfully read work!");
+          } else {
+            console.log("Work read failed.");
+          }
+        } catch (error) {
+          console.error("Error reading work:", error);
+        }
+      };
+
+      handleGet();
+    }, []);
+
+    const {work, file_attachment, date_added } = workData;
+
+    // If file_attachment is available, extract the file name
+    /*const fileName = file_attachment ? file_attachment.split('/').pop() : 'No file attached';
+
+    // If file_attachment is available, create a download link; otherwise, disable the link
+    const downloadLink = file_attachment ? (
+      <a href={file_attachment} download={fileName}>
+        {fileName}
+      </a>
+    ) : (
+      <span>{fileName}</span>
+    );*/
+
     // Update Work
     const [updateWork] = useUpdateWorkMutation();
 
+    const [fileAttachmentError, setFileAttachmentError] = useState(false);
+    const [workError, setWorkError] = useState(false);
+    const [submitButtonClicked, setSubmitButtonClicked] = useState(false);
+
     const handleUpdate = async (e) => {
+
+      setSubmitButtonClicked(true);
+      if (!workData.file_attachment) {
+        setFileAttachmentError(true);
+        return;
+      }
+      if (workData.work.trim() === '') {
+        setWorkError(true);
+        return;
+      }
+  
+      //setFileAttachmentError(false);
+
       const formData = new FormData();
-      formData.append("activity_id", editData.activity_id);
-      formData.append("work", editData.work);
-      formData.append("file_attachment", editData.file_attachment);
+      formData.append("activity_id", id);
+      formData.append("work", workData.work);
+      const fileInput = document.querySelector('input[name="file_attachment"]');
+      const file = fileInput?.files[0];
+
+      // Ensure that file_attachment is a File object
+      if (file instanceof File) {
+        formData.append("file_attachment", file, file.name);
+      }
 
       // Perform validation or additional logic if needed
       console.log("Updating Work:", formData);
@@ -74,19 +150,40 @@ export const EditWorkModal = ({ show, handleClose, editWorkData, onSubmit }) => 
       }
 
       const response = await updateWork({
-          id: editWorkData.id,  // Pass the id for completing the URL
+          id: workId,  // Pass the id for completing the URL
           formData: formData,      // Pass the formData for the request body
       });
 
       console.log(response);
       if (response) {
+        setIsDataChanged(false); // Reset the flag
         handleClose();
         console.log("Successfully updated work!");
       } else {
         console.log("Work update failed.");
       }
 
-      //window.location.reload();
+      window.location.reload();
+    };
+
+    const handleRemoveFile = () => {
+      setWorkData({
+        ...workData,
+        file_attachment: null,
+      });
+
+      setIsDataChanged(true);
+    };
+
+    const handleChange = (e) => {
+      const { name, value, files } = e.target;
+  
+      setWorkData((prevData) => ({
+        ...prevData,
+        [name]: name === "file_attachment" ? (files && files[0]) : value,
+      }));
+
+      setIsDataChanged(true);
     };
 
   
@@ -96,26 +193,42 @@ export const EditWorkModal = ({ show, handleClose, editWorkData, onSubmit }) => 
           <Modal.Title className="fs-6 fw-bold">Edit Work</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form className="d-flex flex-column gap-3">
+          <Form className="d-flex flex-column gap-3" encType="multipart/form-data">
             <Form.Group controlId="work-input">
               <Form.Label>Work</Form.Label>
               <Form.Control
                 type="text"
                 name="work"
-                value={editData?.work}
+                value={workData?.work}
                 onChange={handleChange}
               />
             </Form.Group>
+            {workError && workData.work.trim() === '' && (
+              <p className="text-danger">Please add text to the Work field.</p>
+            )}
 
             <Form.Group controlId="file-input">
               <Form.Label>File Attachment</Form.Label>
-              {fileAttachmentName && (
-                <span>
-                  <br />
-                  {fileAttachmentName}
-                </span>
+              {workData.file_attachment ? (
+                <>
+                  <p className="mb-0 mr-2">{fileAttachmentName}</p>
+                  <Button variant="outline-danger" onClick={handleRemoveFile}>
+                    X 
+                  </Button>
+                </>
+              ) : (
+                <Form.Control
+                  type="file"
+                  name="file_attachment"
+                  onChange={handleChange}
+                />
               )}
+                
             </Form.Group>
+
+            {fileAttachmentError && (
+              <p className="text-danger">Please add a file attachment before updating.</p>
+            )}
 
             {/* Add more Form.Group elements for other fields as needed */}
           </Form>
@@ -127,6 +240,7 @@ export const EditWorkModal = ({ show, handleClose, editWorkData, onSubmit }) => 
           <Button 
             variant="secondary" 
             onClick={handleUpdate}
+            disabled={!isDataChanged} // Disable the button if no changes
           >
             Update Work
           </Button>
