8f1fb34bfd33bcdc7744c2af24393b0ce80b1aa1
CHARIS ARLIE BACLAYON <charisarlie.baclayon@cit.edu>
2023-12-21 04:19:54 +0800

    Allowed host

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
-ALLOWED_HOSTS += os.environ.get('ALLOWED_HOSTS').split()
+ALLOWED_HOSTS += os.environ.get('ALLOWED_HOSTS')
 
 # Application definition
 
